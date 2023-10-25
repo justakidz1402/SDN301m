74701f90aa0ecb66e9975d4fd7fc0858d23eeafb
Justakidz <justakidz1402@gmail.com>
2023-10-25 07:44:29 +0700

    đmm

--- routes/category.route.js
+++ routes/category.route.js
@@ -3,7 +3,7 @@ import categoryController from '../controllers/category.controller.js';
 
 const categoryRouter = express.Router();
 
-categoryRouter.post('/category', categoryController.create);
-categoryRouter.get('/category', categoryController.getAll);
+categoryRouter.post('/', categoryController.create);
+categoryRouter.get('/', categoryController.getAll);
 
 export default categoryRouter;
--- routes/product.route.js
+++ routes/product.route.js
@@ -2,10 +2,10 @@ import express from 'express';
 import controllers from '../controllers/index.js';
 
 const productRouter = express.Router();
-productRouter.post('/product', controllers.productController.create);
-productRouter.get('/product', controllers.productController.getAll);
-productRouter.get('/product/:id', controllers.productController.getDetail);
-productRouter.post('/product/:id/comment', controllers.productController.addComment);
-productRouter.get('/product/:id/comments', controllers.productController.getAllComment);
+productRouter.post('/', controllers.productController.create);
+productRouter.get('/', controllers.productController.getAll);
+productRouter.get('/:id', controllers.productController.getDetail);
+productRouter.post('/comments/:id', controllers.productController.addComment);
+productRouter.get('/comments/:id', controllers.productController.getAllComment);
 
 export default productRouter;
